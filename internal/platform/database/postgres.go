be7e05d275637eb50d8fb78139f7ac29230b2430
Muhuck <etika.gede@gmail.com>
2026-01-28 07:34:29 +0800

    Save supabase configuration connect
    connect supabase

--- internal/platform/database/postgres.go
+++ internal/platform/database/postgres.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"log"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -12,5 +13,16 @@ func Connect(dsn string) *gorm.DB {
 	if err != nil {
 		log.Fatal("failed to connect database", err)
 	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("failed to get sql.DB:", err)
+	}
+
+	//WAJIB untuk Railway + Supabase
+	sqlDB.SetMaxOpenConns(10)                  // maksimal koneksi aktif
+	sqlDB.SetMaxIdleConns(5)                   // koneksi idle
+	sqlDB.SetConnMaxLifetime(30 * time.Minute) // refresh koneksi
+
 	return db
 }
