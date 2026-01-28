c7e58f995e110b3742a112da3c50341777449dee
Muhuck <etika.gede@gmail.com>
2026-01-28 22:42:48 +0800

    Save supabase config

--- internal/product/model.go
+++ internal/product/model.go
@@ -3,10 +3,11 @@ package product
 import "time"
 
 type Product struct {
-	ID        uint      `json:"id"`
-	Name      string    `json:"name"`
-	Price     int       `json:"price"`
-	Stock     int       `json:"stock"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID         uint      `json:"id"`
+	CategoryID uint      `json:"category_id"`
+	Name       string    `json:"name"`
+	Price      int       `json:"price"`
+	Stock      int       `json:"stock"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
 }
